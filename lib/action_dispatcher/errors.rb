6dc55f6dc323635a85c84826ad910e7b833364cd
Néstor Salceda <nestor.salceda@gmail.com>
2017-05-12 12:49:28 +0200

    Add rubocop settings

--- lib/action_dispatcher/errors.rb
+++ lib/action_dispatcher/errors.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 module ActionDispatcher
-  class ActionNotFoundError < StandardError
-  end
+  class ActionNotFoundError < StandardError; end
 
-  class ActionAlreadyExistsError < StandardError
-  end
+  class ActionAlreadyExistsError < StandardError; end
 end
